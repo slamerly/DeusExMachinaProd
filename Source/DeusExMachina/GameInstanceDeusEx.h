8120016d56839c2e02150a9985f64cad319c93ff
Cyril Crozat <ccrozat@artfx.fr>
2024-09-04 16:30:11 +0200

    GPP - add(basic): Game mode
    Created game mode, HUD and Game instance class and blueprints

---
+++ Source/DeusExMachina/GameInstanceDeusEx.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstanceDeusEx.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEUSEXMACHINA_API UGameInstanceDeusEx : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DeusExMachina/HUDDeusEx.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUDDeusEx.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEUSEXMACHINA_API AHUDDeusEx : public AHUD
+{
+	GENERATED_BODY()
+	
+};
